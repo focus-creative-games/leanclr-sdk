142382a9c8f71c6c2675c6409c8219c3d981e44d
walon <walon@code-philosophy.com>
2026-01-26 18:17:53 +0800

    fix: fix bug of managed/build.bat

--- src/managed/build.bat
+++ src/managed/build.bat
@@ -1,5 +1,5 @@
 dotnet build managed.sln -c Debug
-copy WxSdk\bin\Debug\WxSdk.dll ..\samples\flybattle\dlls\WxSdk.dll.bin /Y
-copy WxSdk\bin\Debug\WxSdk.pdb ..\samples\flybattle\dlls\WxSdk.pdb.bin /Y
-copy Game\bin\Debug\Game.dll ..\samples\flybattle\dlls\Game.dll.bin /Y
-copy Game\bin\Debug\Game.pdb ..\samples\flybattle\dlls\Game.pdb.bin /Y
+copy WxSdk\bin\Debug\WxSdk.dll ..\samples\skybattle\dlls\WxSdk.dll.bin /Y
+copy WxSdk\bin\Debug\WxSdk.pdb ..\samples\skybattle\dlls\WxSdk.pdb.bin /Y
+copy Game\bin\Debug\Game.dll ..\samples\skybattle\dlls\Game.dll.bin /Y
+copy Game\bin\Debug\Game.pdb ..\samples\skybattle\dlls\Game.pdb.bin /Y
