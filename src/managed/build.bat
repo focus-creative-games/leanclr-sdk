fcbdad7e392b124b5dc5200c1058dd16722652ad
walon <walon@code-philosophy.com>
2026-01-26 17:39:37 +0800

    fix: fix bug of managed/build.bat that use old name LeanCLRSDK

--- src/managed/build.bat
+++ src/managed/build.bat
@@ -1,5 +1,5 @@
 dotnet build managed.sln -c Debug
-copy LeanCLRSdk\bin\Debug\LeanCLRSdk.dll ..\samples\flybattle\dlls\LeanCLRSdk.dll.bin /Y
-copy LeanCLRSdk\bin\Debug\LeanCLRSdk.pdb ..\samples\flybattle\dlls\LeanCLRSdk.pdb.bin /Y
+copy WxSdk\bin\Debug\WxSdk.dll ..\samples\flybattle\dlls\WxSdk.dll.bin /Y
+copy WxSdk\bin\Debug\WxSdk.pdb ..\samples\flybattle\dlls\WxSdk.pdb.bin /Y
 copy Game\bin\Debug\Game.dll ..\samples\flybattle\dlls\Game.dll.bin /Y
 copy Game\bin\Debug\Game.pdb ..\samples\flybattle\dlls\Game.pdb.bin /Y
